42bbe17f2193e2ac8b54cb40c443c08ca8a4f391
SOGANG <SOGANG@DESKTOP-T84A3NC>
2022-02-24 11:28:37 +0900

    day3 add

--- start/src/day02/Ex02.java
+++ start/src/day02/Ex02.java
@@ -1,12 +1,9 @@
-package day02
+package day02;
 
-import javax.swing.JOptionPane;
-
+import javax.swing.*;
 public class Ex02 {
 	public static void main(String[] args) {
 		String str = JOptionPane.showInputDialog("만나고 싶은 가수를 입력하세요!");
-		System.out.println(str);
-		JOptionPane.showMessageDialog(null, "당신이 만나고 싶은 가수는 " + str + "입니다.");
+		JOptionPane.showMessageDialog(null, "당신이 만나고 싶은 가수는 "  + str + " 입니다.");
 	}
-
 }
--- start/src/day03/Test01.java
+++ start/src/day03/Test01.java
@@ -1,5 +1,8 @@
 package day03;
 
 public class Test01 {
+	public static void main(String[] args) {
+		System.out.println("깃허브에 추가 하기");
+	}
 
 }
