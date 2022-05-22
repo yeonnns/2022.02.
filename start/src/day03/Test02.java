e4648bc1a832abceed198485bc75d31792a583b7
yeonnns <100174595+yeonnns@users.noreply.github.com>
2022-05-22 11:53:17 +0900

    Delete start/src directory

--- start/src/day03/Test02.java
+++
@@ -1,9 +0,0 @@
-package day03;
-
-public class Test02 {
-	public static void main(String[] args) {
-		System.out.println("3.14 + 10 : " + 3.14 + 10);
-		System.out.println("3.14 + 10 : " + (3.14 + 10));
-	}
-
-}
